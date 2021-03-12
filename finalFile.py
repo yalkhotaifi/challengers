008f6b80c1925a118ec5ff390957f9d165eb0169
yasseradel87 <71362647+yasseradel87@users.noreply.github.com>
2021-03-12 15:23:23 -0800

    finalfile is edited

--- finalFile.py
+++ finalFile.py
@@ -1,3 +1,23 @@
+# a) Write a program containing a function to reverse a user inputted string.
+
+string=input()
+stringlength = len(string)
+reversedString = string[stringlength::-1] 
+print(slicedString)
+
+# b) Write a program containing a function to check if a user inputted number is prime
+
+num = int(input())
+if num > 1:
+for i in range(2,num):
+if (num % i) == 0:
+print(num,"is not a prime number")
+break
+else:
+print(num,"is a prime number")
+else:
+print(num,"is not a prime number")
+
 #C) Write a program containing a function to convert the current time, into military time
 
 import datetime
