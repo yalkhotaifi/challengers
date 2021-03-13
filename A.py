b8045e3c03002cf0bc0f1452b1789c8e965965b3
yasseradel187 <80550805+yasseradel187@users.noreply.github.com>
2021-03-12 20:09:58 -0800

    Add files via upload

---
+++ A.py
@@ -0,0 +1,7 @@
+# a) Write a program containing a function to reverse a user inputted string.
+
+string=input()
+stringlength = len(string)
+reversedString = string[stringlength::-1] 
+print(slicedString)
+
